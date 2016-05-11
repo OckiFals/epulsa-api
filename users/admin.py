41a66ab8164a73fa9e363d417d0505fd019270ba
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2016-05-11 19:15:53 +0700

    Add token app on admin site url

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from users.models import Customer
+from rest_framework.authtoken.admin import TokenAdmin
 
 
 class CustomerAdmin(admin.ModelAdmin):
@@ -7,5 +8,5 @@ class CustomerAdmin(admin.ModelAdmin):
     # list_display = ('', 'brewery', 'locality',)
     search_fields = ['phone']
 
-
+TokenAdmin.raw_id_fields = ('user',)
 admin.site.register(Customer, CustomerAdmin)
