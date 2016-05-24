c10c06e92fc539428c065475627e87f58a16d41c
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2016-05-24 11:04:18 +0700

    Add custome validation on LoginForm

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,19 @@
 from django import forms
+from django.contrib.auth import authenticate
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label=u'Username')
     password = forms.CharField(label=u'Password',
                                widget=forms.PasswordInput(render_value=False))
+
+    def clean(self):
+        user = authenticate(
+            username=self.cleaned_data['username'],
+            password=self.cleaned_data['password']
+        )
+
+        if user is not None:
+            self.user = user
+            return self.cleaned_data
+        raise forms.ValidationError("Unable to login with provided credentials.")
