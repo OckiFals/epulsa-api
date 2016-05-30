187aaabc65b5b3e381fced1f86c5849cf30433e7
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2016-05-30 16:17:11 +0700

    Add counter and admin url rules

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,16 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from users import views
 
 urlpatterns = [
-    url(r'^customer/$', views.CustomerList.as_view()),
+    # customer
+    url(r'^customer/$', views.CustomerList.as_view(), name="customer-all"),
     url(r'^customer/(?P<pk>[0-9]+)/$', views.CustomerDetail.as_view(), name="customer-detail"),
+    # counter
+    url(r'^counter/$', views.CounterList.as_view(), name="counter-all"),
+    url(r'^counter/(?P<pk>[0-9]+)/$', views.CounterDetail.as_view(), name="counter-detail"),
+    # admin
+    url(r'^admin/$', views.AdminList.as_view(), name="admin-all"),
+    url(r'^admin/(?P<pk>[0-9]+)/$', views.AdminDetail.as_view(), name="admin-detail"),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
